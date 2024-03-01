feed207bce3286676d62c8e8ff2a1899367a93cf
abelsilvabrandao <abelsilvabrandao@hotmal.com>
2024-03-01 16:49:14 -0300

    guardando projeto

--- script.js
+++ script.js
@@ -7,3 +7,13 @@ function toggleMode() {
   //}
   html.classList.toggle("light")
 }
+
+function openForm() {
+  var modal = document.getElementById("form-modal")
+  modal.style.display = "block"
+}
+
+function closeForm() {
+  var modal = document.getElementById("form-modal")
+  modal.style.display = "none"
+}
